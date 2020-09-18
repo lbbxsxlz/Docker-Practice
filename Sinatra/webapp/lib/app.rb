02f88999064de2409bfbc400246dc6acb9107ece
BinbinLee <lbbxsxlz@gmail.com>
2020-09-18 17:22:52 +0800

    update

--- Sinatra/webapp/lib/app.rb
+++ Sinatra/webapp/lib/app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Application
   set :bind, '0.0.0.0'
 
   get '/' do
-    "<h1>DockerBook Test Sinatra app</h1>"
+    "<h1>My first Docker Test Sinatra app</h1>"
   end
 
   post '/json/?' do

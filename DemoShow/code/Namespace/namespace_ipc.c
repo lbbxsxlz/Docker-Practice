1e5d7b384f1ba3a518c701d9755df5a35f97ed40
BinbinLee <lbbxsxlz@gmail.com>
2021-01-28 20:15:00 +0800

    update Path Name and add new test file

---
+++ DemoShow/code/Namespace/namespace_ipc.c
@@ -0,0 +1,36 @@
+#define _GNU_SOURCE
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mount.h>
+#include <stdio.h>
+#include <sched.h>
+#include <signal.h>
+#include <unistd.h>
+
+#define STACK_SIZE (1024 * 1024)
+
+static char container_stack[STACK_SIZE];
+char* const container_args[] = {
+    "/bin/bash",
+    NULL
+};
+
+int container_main(void* arg)
+{
+	printf("Test-Container - inside the container!\n");
+	sethostname("lbbxsxlz-container",19);
+	execv(container_args[0], container_args);
+	printf("Something's wrong!\n");
+	return 1;
+}
+
+int main()
+{
+	printf("Parent - start a container!\n");
+	/*启用CLONE_NEWUTS、CLONE_NEWIPC Namespace隔离 */
+	int container_pid = clone(container_main, container_stack+STACK_SIZE, 
+		CLONE_NEWUTS | CLONE_NEWIPC | SIGCHLD, NULL); 
+	waitpid(container_pid, NULL, 0);
+	printf("Parent - container stopped!\n");
+	return 0;
+}

64d39a387858fc96154ef7f407fdf608bbde0ba2
BinbinLee <lbbxsxlz@gmail.com>
2021-12-12 22:27:02 +0800

    update code

--- DemoShow/code_golang/namespaceTest.go
+++ DemoShow/code_golang/namespaceTest.go
@@ -10,15 +10,14 @@ import (
 func main(){
 	cmd:=exec.Command("sh")
 	cmd.SysProcAttr=&syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNET,
 	}
-        //让指定的系统用户去打开子进程
-	cmd.SysProcAttr.Credential=&syscall.Credential{Uid:uint32(1),Gid:uint32(1)}
+	//cmd.SysProcAttr.Credential=&syscall.Credential{Uid:uint32(1),Gid:uint32(1)}
 	cmd.Stdin=os.Stdin
 	cmd.Stdout=os.Stdout
 	cmd.Stdin=os.Stdin
 	cmd.Stderr=os.Stderr
-	if err:=cmd.Run();err!=nil {
+	if err:=cmd.Run();err!=nil{
 		log.Fatal(err)
 	}
 	os.Exit(-1)
